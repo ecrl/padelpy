95af693346548d3a22e3e691e35c3fcdb1706785
Kessler <Travis_Kessler@student.uml.edu>
2019-06-07 13:02:10 -0400

    Version bump: 0.1.1 -> 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='padelpy',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python wrapper for PaDEL-Descriptor',
     url='https://github.com/ecrl/padelpy',
     author='Travis Kessler',
