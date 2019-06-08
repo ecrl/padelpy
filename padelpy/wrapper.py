ccd449251e95469f828c484c299df3697a486fb2
tjkessler <travis.j.kessler@gmail.com>
2019-06-08 11:47:32 -0400

    Version bump: 0.1.2 -> 0.1.3

--- padelpy/wrapper.py
+++ padelpy/wrapper.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # padelpy/wrapper.py
-# v.0.1.2
+# v.0.1.3
 # Developed in 2019 by Travis Kessler <travis.j.kessler@gmail.com>
 #
 # Contains the `padeldescriptor` function, a wrapper for PaDEL-Descriptor
