059c7aac7117ddaaaee2f5be9e8bc128e214d2b2
Travis Kessler <travis.j.kessler@gmail.com>
2021-04-13 10:46:54 -0400

    Version bump: 0.1.8 -> 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='padelpy',
-    version='0.1.8',
+    version='0.1.9',
     description='A Python wrapper for PaDEL-Descriptor',
     url='https://github.com/ecrl/padelpy',
     author='Travis Kessler',
