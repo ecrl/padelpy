c7df17747250a2e50f98b21c32581b34f0721943
Fanwang Meng <fwmeng88@gmail.com>
2021-11-09 12:28:15 -0500

    Add support of SDF file

--- padelpy/__init__.py
+++ padelpy/__init__.py
@@ -1,3 +1,3 @@
 from padelpy.wrapper import padeldescriptor
-from padelpy.functions import from_mdl, from_smiles
+from padelpy.functions import from_mdl, from_smiles, from_sdf
 __version__ = '0.1.10'
