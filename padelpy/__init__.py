0e5d37f692e49caf2f981de1590e45fa25ec0e20
tjkessler <travis.j.kessler@gmail.com>
2022-12-10 16:08:40 -0500

    retainorder argument now defaults to true; bump 0.1.12 -> 0.1.13

--- padelpy/__init__.py
+++ padelpy/__init__.py
@@ -1,3 +1,3 @@
 from .wrapper import padeldescriptor
 from .functions import from_mdl, from_smiles, from_sdf
-__version__ = '0.1.10'
+__version__ = '0.1.13'
--- padelpy/version.py
+++ padelpy/version.py
@@ -1,4 +1,4 @@
-VERSION = (0, 1, 12, "")
+VERSION = (0, 1, 13, "")
 
 __version__ = ".".join(map(str, VERSION[:-1]))
 __release__ = ".".join(map(str, VERSION))
